2b02651e1533611c09cdbe05b9aad36957d3117e
Dustin J. <djourdan555@gmail.com>
2026-04-07 21:00:39 +0700

    Add: three-integer-sum - submission-0

---
+++ Data Structures & Algorithms/three-integer-sum/submission-0.go
@@ -0,0 +1,41 @@
+func threeSum(nums []int) [][]int {
+    sort.Ints(nums)
+    count := make(map[int]int)
+    for _, num := range nums {
+        count[num]++
+    }
+
+    var res [][]int
+    for i := 0; i < len(nums); i++ {
+        count[nums[i]]--
+        if i > 0 && nums[i] == nums[i-1] {
+            continue
+        }
+        for j := i + 1; j < len(nums); j++ {
+            count[nums[j]]--
+            if j > i+1 && nums[j] == nums[j-1] {
+                continue
+            }
+            target := -(nums[i] + nums[j])
+            if count[target] > 0 {
+                res = append(res, []int{nums[i], nums[j], target})
+            }
+        }
+        for j := i + 1; j < len(nums); j++ {
+            count[nums[j]]++
+        }
+    }
+    return res
+}
+
+func twoSum(nums []int, target int) []int {
+    numMap := make(map[int]int, len(nums))
+    for i, num := range nums {
+        complement := target - num
+        if j, ok := numMap[complement]; ok {
+            return []int{j, i}
+        }
+        numMap[num] = i
+    }
+    return []int{}
+}
